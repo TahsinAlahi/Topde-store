c825a1b7072470ceda89da32b8a888a29f8ca3aa
Tahsin <alahitahsin@gmail.com>
2024-10-09 09:43:19 +0600

    added some hooks on ProductContext

--- src/Contexts/ProductContext.tsx
+++ src/Contexts/ProductContext.tsx
@@ -1,10 +1,51 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
+import { ProductType } from "../Hooks/useFetchProducts";
+
+interface ProductState {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  productInCurt: ProductType[] | any[];
+}
+
+interface ProductAction {
+  type: string;
+  payload: unknown;
+}
+
+const initialState: ProductState = {
+  productInCurt: [],
+};
+
+function reducer(state: ProductState, action: ProductAction) {
+  switch (action.type) {
+    case "add_to_cart":
+      return {
+        ...state,
+        productInCurt: [...state.productInCurt, action.payload],
+      };
+    case "remove_from_cart":
+      return {
+        ...state,
+        productInCurt: state.productInCurt.filter(
+          (product) => product.id !== action.payload
+        ),
+      };
+
+    default:
+      throw new Error("Invalid action type");
+  }
+}
 
 const ProductContext = createContext<unknown>(null);
 
 function ProductProvider({ children }: { children: React.ReactNode }) {
+  const [state, dispatch] = useReducer<
+    React.Reducer<ProductState, ProductAction>
+  >(reducer, initialState);
+
   return (
-    <ProductContext.Provider value={null}>{children}</ProductContext.Provider>
+    <ProductContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ProductContext.Provider>
   );
 }
 
